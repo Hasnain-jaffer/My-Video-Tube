188e04f3142e9c8dbc43e292729b153fb0ffa3fb
Hasnain.jaffer <hasnainjaffer65@gmail.com>
2026-07-11 17:23:48 +0500

    Add register controller and some bug fixes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,82 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/apiError.js';
+import { ApiResponse } from '../utils/apiResponse.js';
+import { User } from '../models/userModel.js';
+import { uploadToCloudinary } from '../utils/cloudinary.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'User registered successfully' });  
-})
+    // get user data from request body
+    // validate user data
+    // check if user already exists,username,email
+    // check images, but check  avatar
+    // upload images to cloudinary
+    // create user in database
+    // remove password and refresh token from user object
+    // check if user is created successfully
+    // send response with user data and success message 
+
+    const { username, email, password, fullName } = req.body;
+    console.log('User data:', { username, email, password, fullName });
+    
+    // .some() method checks if at least one element in the array passes the test implemented by the provided function. In this case, it checks if any of the fields are empty strings after trimming whitespace.
+    if ([username, email, password, fullName].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    // $or operator is used to find a document that matches any of the specified conditions. In this case, it checks if there is an existing user with either the same username or email.
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+        throw new ApiError(400, "Username or email already exists");
+    }
+    
+    // Check if avatar file is provided in the request. If not, throw an error indicating that the avatar file is required.
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+    // Check if cover image file is provided in the request. If not, it will be handled later when creating the user.
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required");
+    }
+    
+    // Upload avatar and cover image to Cloudinary using the provided utility function. The function returns an object containing the URL of the uploaded image.
+    const avatar = await uploadToCloudinary(avatarLocalPath);
+    const coverImage = await uploadToCloudinary(coverImageLocalPath);
+
+    if(!avatar) {
+        throw new ApiError(500, "Failed to upload avatar to Cloudinary");
+    }
+    
+    // Create a new user in the database with the provided data and the URLs of the uploaded images. The cover image URL is optional, so it uses a conditional operator to set it to an empty string if not provided.
+    const newUser = await User.create({
+        username : username.toLowerCase(),
+        email,
+        password,
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    });
+
+    // Fetch the newly created user from the database, excluding the password and refresh token fields for security reasons. This ensures that sensitive information is not sent back in the response.
+    const createdUser = await User.findById(newUser._id).select("-password -refreshToken");
+
+    if (!createdUser) {
+        throw new ApiError(500, "Failed to create user");
+    }
+
+    res.status(201).json(new ApiResponse(200, "User registered successfully", {
+        user: {
+            id: newUser._id,    
+        username: newUser.username, 
+        email: newUser.email,
+        fullName: newUser.fullName,
+        avatar: newUser.avatar,
+        coverImage: newUser.coverImage,
+        createdAt: newUser.createdAt,
+        updatedAt: newUser.updatedAt,
+        }
+    }));
+
+    });
+
 
 export { registerUser };
--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -8,6 +8,6 @@ const storage = multer.diskStorage({
         cb(null, file.originalname) // Specify the filename for uploaded files
     }
 });
-const upload = multer({ 
+export const upload = multer({ 
     storage,
  });   
